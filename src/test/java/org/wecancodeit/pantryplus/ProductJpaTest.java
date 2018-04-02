fb8fe9c52b0c07c777499c6f9deb70279a32f6d2
marcbgold <marcbgold2@gmail.com>
2018-04-02 14:25:10 -0400

    committing before pulling merged master

--- src/test/java/org/wecancodeit/pantryplus/ProductJpaTest.java
+++ src/test/java/org/wecancodeit/pantryplus/ProductJpaTest.java
@@ -15,30 +15,30 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @DataJpaTest
 public class ProductJpaTest {
-	
+
 	@Resource
 	private TestEntityManager entityManager;
-	
+
 	@Resource
 	private CategoryRepository categoryRepo;
-	
+
 	@Resource
 	private ProductRepository productRepo;
-	
+
 	@Test
 	public void shouldSaveAndLoadCategory() {
 		Category underTest = new Category("Fruit");
 		underTest = categoryRepo.save(underTest);
 		long categoryId = underTest.getId();
-		
+
 		entityManager.flush();
 		entityManager.clear();
-		
+
 		underTest = categoryRepo.findOne(categoryId);
-		
-		assertThat(underTest.getName(), is("Fruit"));		
+
+		assertThat(underTest.getName(), is("Fruit"));
 	}
-	
+
 	@Test
 	public void shouldSaveAndLoadProduct() {
 		Category category = new Category("Fruit");
@@ -46,15 +46,15 @@ public class ProductJpaTest {
 		category = categoryRepo.save(category);
 		underTest = productRepo.save(underTest);
 		long productId = underTest.getId();
-		
+
 		entityManager.flush();
 		entityManager.clear();
-		
+
 		underTest = productRepo.findOne(productId);
-		
-		assertThat(underTest.getName(), is("pineapple"));		
+
+		assertThat(underTest.getName(), is("pineapple"));
 	}
-	
+
 	@Test
 	public void shouldSaveManyProductsToOneCategory() {
 		Category category = new Category("fruit");
@@ -64,16 +64,13 @@ public class ProductJpaTest {
 		underTest1 = productRepo.save(underTest1);
 		underTest2 = productRepo.save(underTest2);
 		long categoryId = category.getId();
-		
+
 		entityManager.flush();
 		entityManager.clear();
-		
+
 		category = categoryRepo.findOne(categoryId);
-		
+
 		assertThat(category.getProducts(), containsInAnyOrder(underTest1, underTest2));
-		
 	}
-	
-	
 
 }
