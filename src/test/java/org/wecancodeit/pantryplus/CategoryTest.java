f2674e01f70c8a1bace02bd79263c0cb1f431663
Micheal <mquenti3@gmail.com>
2018-04-02 11:51:07 -0400

    Linked Category and Product

--- src/test/java/org/wecancodeit/pantryplus/CategoryTest.java
+++ src/test/java/org/wecancodeit/pantryplus/CategoryTest.java
@@ -1,15 +1,28 @@
 package org.wecancodeit.pantryplus;
 
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+import java.util.Collection;
 
 import org.junit.Test;
 
 public class CategoryTest {
-	Category testCategory = new Category("testName");
+	Product testProduct = new Product("testProduct");
+	Category testCategory = new Category("testName", testProduct);
 
 	@Test
 	public void shouldGetName() {
-		String check = testCategory.getName();
-		assertEquals(check, "testName");
+		String checkName = testCategory.getName();
+		assertEquals(checkName, "testName");
+	}
+
+	@Test
+	public void shouldHaveProductInCategory() {
+		Collection<Product> checkProduct = testCategory.getProducts();
+		assertThat(testCategory.getProducts(), is(checkProduct));
 	}
 }
