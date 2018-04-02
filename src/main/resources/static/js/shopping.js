66cd89439d53a247a3765ba746057754834edea5
Alex Malcolm <alexjamesmalcolm@gmail.com>
2018-04-02 15:52:47 -0400

    Added event listener for toggleItemsDisplay()

--- src/main/resources/static/js/shopping.js
+++ src/main/resources/static/js/shopping.js
@@ -1,3 +1,12 @@
-function initialize() {}
+function initialize() {
+	const manyCategoryItemsDivs = document.querySelectorAll(".category-items");
+	for (var i = 0; i < manyCategoryItemsDivs.length; i++) {
+		const categoryDiv = manyCategoryItemsDivs[i];
+		categoryDiv.addEventListener("click", () => {
+			toggleItemsDisplay();
+		});
+	}
+	console.log(manyCategoryItemsDivs)
+}
 
 function toggleItemsDisplay() {}
