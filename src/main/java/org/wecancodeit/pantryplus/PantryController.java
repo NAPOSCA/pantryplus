c8f00c599408ff54c2e1a291ef8ff2afceac2085
JDSalisbury <Salisbury.jeffery@gmail.com>
2018-04-03 13:52:51 -0400

    non-restfull controller added

---
+++ src/main/java/org/wecancodeit/pantryplus/PantryController.java
@@ -0,0 +1,21 @@
+package org.wecancodeit.pantryplus;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PantryController {
+
+	@Resource
+	private CategoryRepository categoryRepo;
+	
+	@RequestMapping("/index")
+	public String displayMainPage(Model model) {
+		model.addAttribute("categories", categoryRepo.findAll());
+		return "index";
+	}
+	
+}
