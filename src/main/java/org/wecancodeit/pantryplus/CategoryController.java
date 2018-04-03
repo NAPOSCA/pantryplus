c16fe2f51dd97aef4f45423796937ca7649bfaf5
JDSalisbury <Salisbury.jeffery@gmail.com>
2018-04-03 11:36:24 -0400

    added Controller for Category

--- src/main/java/org/wecancodeit/pantryplus/CategoryController.java
+++ src/main/java/org/wecancodeit/pantryplus/CategoryController.java
@@ -1,8 +1,20 @@
 package org.wecancodeit.pantryplus;
 
+import javax.annotation.Resource;
+
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CategoryController {
+
+	@Resource
+	private CategoryRepository categoryRepo;
+
+	@RequestMapping("/index")
+	public Iterable<Category> findAllCategories() {
+
+		return categoryRepo.findAll();
+	}
 
 }
