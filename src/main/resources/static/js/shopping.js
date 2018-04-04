89b0adc64eef4341e5a0b5c79f9f7fbe97f66b5e
Alex Malcolm <alexjamesmalcolm@gmail.com>
2018-04-04 10:26:07 -0400

    Changed var to let

--- src/main/resources/static/js/shopping.js
+++ src/main/resources/static/js/shopping.js
@@ -1,6 +1,6 @@
 function initialize() {
 	const manyCategoryItemsDivs = document.querySelectorAll(".category-items");
-	for (var i = 0; i < manyCategoryItemsDivs.length; i++) {
+	for (let i = 0; i < manyCategoryItemsDivs.length; i++) {
 		const categoryDiv = manyCategoryItemsDivs[i];
 		const items = categoryDiv.querySelector(".items");
 		categoryDiv.addEventListener("click", () => {
