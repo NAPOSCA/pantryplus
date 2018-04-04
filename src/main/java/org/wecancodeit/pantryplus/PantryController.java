5e8c996800c7c6c367090334d07acfc0d38ce1a6
JDSalisbury <Salisbury.jeffery@gmail.com>
2018-04-04 11:40:26 -0400

    Added Cart Model to PantryController

--- src/main/java/org/wecancodeit/pantryplus/PantryController.java
+++ src/main/java/org/wecancodeit/pantryplus/PantryController.java
@@ -5,9 +5,13 @@ import javax.annotation.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PantryController {
+	
+	@Resource
+	private CartRepository cartRepo;
 
 	@Resource
 	private CategoryRepository categoryRepo;
@@ -15,6 +19,7 @@ public class PantryController {
 	@RequestMapping("/")
 	public String displayMainPage(Model model) {
 		model.addAttribute("categories", categoryRepo.findAll());
+		model.addAttribute("cart", cartRepo.findOne(1L));
 		return "index";
 	}
 	
