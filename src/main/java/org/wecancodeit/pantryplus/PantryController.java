1ec69f2bba20e822d83658958ace92c61f99061d
JDSalisbury <Salisbury.jeffery@gmail.com>
2018-04-04 11:44:48 -0400

    cleaned up

--- src/main/java/org/wecancodeit/pantryplus/PantryController.java
+++ src/main/java/org/wecancodeit/pantryplus/PantryController.java
@@ -5,22 +5,21 @@ import javax.annotation.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PantryController {
-	
+
 	@Resource
 	private CartRepository cartRepo;
 
 	@Resource
 	private CategoryRepository categoryRepo;
-	
+
 	@RequestMapping("/")
 	public String displayMainPage(Model model) {
 		model.addAttribute("categories", categoryRepo.findAll());
 		model.addAttribute("cart", cartRepo.findOne(1L));
 		return "index";
 	}
-	
+
 }
