0bae94f9f43b83f6b5ea8567b4c21552c183a5f2
Alex Malcolm <alexjamesmalcolm@gmail.com>
2018-04-05 15:46:16 -0400

    Removed unneeded import

--- src/test/java/org/wecancodeit/pantryplus/PantryControllerMockMvcTest.java
+++ src/test/java/org/wecancodeit/pantryplus/PantryControllerMockMvcTest.java
@@ -10,7 +10,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
