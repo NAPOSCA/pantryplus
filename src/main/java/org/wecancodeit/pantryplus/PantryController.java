6cad354105bfddc0198f123171e64d2a6fe9c399
Alex Malcolm <alexjamesmalcolm@gmail.com>
2018-04-06 10:39:35 -0400

    shouldHaveDisplayShoppingAddCategoriesAndCartToModel

--- src/main/java/org/wecancodeit/pantryplus/PantryController.java
+++ src/main/java/org/wecancodeit/pantryplus/PantryController.java
@@ -1,16 +1,26 @@
 package org.wecancodeit.pantryplus;
 
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
 @Controller
 public class PantryController {
+	
+	@Resource
+	private CartRepository cartRepo;
+	
+	@Resource
+	private CategoryRepository categoryRepo;
 
 	public String displayUserForm(Model model) {
 		return "user-form";
 	}
 
 	public String displayShopping(Model model) {
+		model.addAttribute("categories", categoryRepo.findAll());
+		model.addAttribute("cart", cartRepo.findOne(1L));
 		return "shopping";
 	}
 
--- src/test/java/org/wecancodeit/pantryplus/PantryControllerTest.java
+++ src/test/java/org/wecancodeit/pantryplus/PantryControllerTest.java
@@ -1,7 +1,10 @@
 package org.wecancodeit.pantryplus;
 
+import static java.util.Arrays.asList;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -17,6 +20,21 @@ public class PantryControllerTest {
 	
 	@Mock
 	private Model model;
+	
+	@Mock
+	private Category category;
+	
+	@Mock
+	private Category anotherCategory;
+	
+	@Mock
+	private CategoryRepository categoryRepo;
+
+	@Mock
+	private CartRepository cartRepo;
+
+	@Mock
+	private Cart cart;
 	
 	@Before
 	public void setup() {
@@ -39,5 +57,18 @@ public class PantryControllerTest {
 	public void shouldHaveDisplayCartReturnCart() {
 		String templateName = underTest.displayCart(model);
 		assertThat(templateName, is("cart"));
+	}
+	
+	@Test
+	public void shouldHaveDisplayShoppingAddCategoriesAndCartToModel() {
+		Iterable<Category> categories = asList(category, anotherCategory);
+		when(categoryRepo.findAll()).thenReturn(categories);
+		long cartId = 1L;
+		when(cartRepo.findOne(cartId)).thenReturn(cart);
+		
+		underTest.displayShopping(model);
+		
+		verify(model).addAttribute("categories", categories);
+		verify(model).addAttribute("cart", cart);
 	}
 }
