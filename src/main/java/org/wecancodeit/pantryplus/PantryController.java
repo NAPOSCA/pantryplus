b0c6b26b99f3226e8c2f2e9f243fb757c185e287
Alex Malcolm <alexjamesmalcolm@gmail.com>
2018-04-06 10:24:44 -0400

    shouldHaveDisplayUserFormReturnUserForm

--- src/main/java/org/wecancodeit/pantryplus/PantryController.java
+++ src/main/java/org/wecancodeit/pantryplus/PantryController.java
@@ -1,31 +1,37 @@
 package org.wecancodeit.pantryplus;
 
-import javax.annotation.Resource;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PantryController {
 
-	@Resource
-	private CartRepository cartRepo;
-
-	@Resource
-	private CategoryRepository categoryRepo;
-
-	@RequestMapping("/")
-	public String displayMainPage(Model model) {
-		model.addAttribute("categories", categoryRepo.findAll());
-		model.addAttribute("cart", cartRepo.findOne(1L));
-		return "shopping";
+	public String displayUserForm(Model model) {
+		return "user-form";
 	}
 
-	@RequestMapping("/cart")
-	public String displayCart(Model model) {
-		model.addAttribute("cart", cartRepo.findOne(1L));
-		return "cart";
-	}
+	// @Resource
+	// private CartRepository cartRepo;
+	//
+	// @Resource
+	// private CategoryRepository categoryRepo;
+
+	// @RequestMapping("/shopping")
+	// public String displayMainPage(Model model) {
+	// model.addAttribute("categories", categoryRepo.findAll());
+	// model.addAttribute("cart", cartRepo.findOne(1L));
+	// return "shopping";
+	// }
+	//
+	// @RequestMapping("/cart")
+	// public String displayCart(Model model) {
+	// model.addAttribute("cart", cartRepo.findOne(1L));
+	// return "cart";
+	// }
+	//
+	// @RequestMapping("/")
+	// public String displayUserForm(Model model) {
+	// return "user-form";
+	// }
 
 }
---
+++ src/test/java/org/wecancodeit/pantryplus/PantryControllerTest.java
@@ -0,0 +1,31 @@
+package org.wecancodeit.pantryplus;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+
+public class PantryControllerTest {
+	
+	@InjectMocks
+	private PantryController underTest;
+	
+	@Mock
+	private Model model;
+	
+	@Before
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+	}
+	
+	@Test
+	public void shouldHaveDisplayUserFormReturnUserForm() {
+		String templateName = underTest.displayUserForm(model);
+		assertThat(templateName, is("user-form"));
+	}
+}
