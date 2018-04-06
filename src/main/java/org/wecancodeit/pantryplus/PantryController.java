2dbedf3e99aa416cddc206ad7bb31bc1d59a64e5
theoccasionalist <mrmccall07@gmail.com>
2018-04-06 11:45:01 -0400

    removed comments

--- src/main/java/org/wecancodeit/pantryplus/PantryController.java
+++ src/main/java/org/wecancodeit/pantryplus/PantryController.java
@@ -32,28 +32,4 @@ public class PantryController {
 		return "cart";
 	}
 
-	// @Resource
-	// private CartRepository cartRepo;
-	//
-	// @Resource
-	// private CategoryRepository categoryRepo;
-
-	// @RequestMapping("/shopping")
-	// public String displayMainPage(Model model) {
-	// model.addAttribute("categories", categoryRepo.findAll());
-	// model.addAttribute("cart", cartRepo.findOne(1L));
-	// return "shopping";
-	// }
-	//
-	// @RequestMapping("/cart")
-	// public String displayCart(Model model) {
-	// model.addAttribute("cart", cartRepo.findOne(1L));
-	// return "cart";
-	// }
-	//
-	// @RequestMapping("/")
-	// public String displayUserForm(Model model) {
-	// return "user-form";
-	// }
-
 }
