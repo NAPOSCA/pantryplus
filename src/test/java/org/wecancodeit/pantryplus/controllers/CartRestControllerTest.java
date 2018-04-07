b44862f5fef673d841db57eb54ab326d63f90b02
Alex Malcolm <alexjamesmalcolm@gmail.com>
2018-04-07 14:31:13 -0400

    Removed unrelated test

--- src/test/java/org/wecancodeit/pantryplus/controllers/CartRestControllerTest.java
+++ src/test/java/org/wecancodeit/pantryplus/controllers/CartRestControllerTest.java
@@ -1,17 +1,11 @@
 package org.wecancodeit.pantryplus.controllers;
 
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
-import static org.mockito.Mockito.when;
-
 import org.junit.Before;
-import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.wecancodeit.pantryplus.cart.Cart;
 import org.wecancodeit.pantryplus.cart.CartRepository;
-import org.wecancodeit.pantryplus.controllers.CartRestController;
 import org.wecancodeit.pantryplus.product.ProductRepository;
 
 public class CartRestControllerTest {
@@ -32,28 +26,5 @@ public class CartRestControllerTest {
 	public void setup() {
 		MockitoAnnotations.initMocks(this);
 	}
-
-	@Test
-	public void shouldRetrieveCart() {
-		long id = 34L;
-		when(cartRepo.findOne(id)).thenReturn(cart);
-
-		Cart result = cartRepo.findOne(id);
-
-		assertThat(result, is(cart));
-	}
-
-	// @Test
-	// public void shouldAddNewLineItemToCart() {
-	// long cartId = 34L;
-	// when(cartRepo.findOne(cartId)).thenReturn(cart);
-	// long productId = 1L;
-	// when(productRepo.findOne(productId)).thenReturn(product);
-	//
-	//
-	// Cart result = cartRepo.findOne(cartId);
-	//
-	// assertThat(result, is(cart));
-	// }
 
 }
