0adab293f99f0c96609ce9a78ec981b5bf9871a5
alexjamesmalcolm <alexjamesmalcolm@gmail.com>
2018-04-28 20:46:56 -0400

    Fixed imports

--- src/test/java/org/wecancodeit/pantryplus/controllers/EmailControllerMockTest.java
+++ src/test/java/org/wecancodeit/pantryplus/controllers/EmailControllerMockTest.java
@@ -2,14 +2,7 @@ package org.wecancodeit.pantryplus.controllers;
 
 import static org.mockito.Mockito.verify;
 
-<<<<<<< HEAD
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.mail.internet.MimeMessage;
-=======
 import javax.mail.MessagingException;
->>>>>>> fe754d6ea01ac9e7daa44e6e041a523abe404b17
 
 import org.junit.Before;
 import org.junit.Test;
