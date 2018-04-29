0891ac5e48152a8e418c775ff09536af687fa212
alexjamesmalcolm <alexjamesmalcolm@gmail.com>
2018-04-28 21:58:03 -0400

    Tested more of the EmailController

--- src/test/java/org/wecancodeit/pantryplus/controllers/EmailControllerMockTest.java
+++ src/test/java/org/wecancodeit/pantryplus/controllers/EmailControllerMockTest.java
@@ -1,9 +1,11 @@
 package org.wecancodeit.pantryplus.controllers;
 
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.util.Map;
 
@@ -11,6 +13,7 @@ import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 
 import org.hamcrest.Matchers;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
@@ -36,12 +39,12 @@ public class EmailControllerMockTest {
 	@Mock
 	private MimeMessageHelper helper;
 
-	private Map<String, Object> model;
+	private Map<String, Object> cartModel;
 
 	private MimeMessage message;
 	
 	@Mock
-	Model model1;
+	Model springModel;
 
 	@Before
 	public void setup() {
@@ -76,22 +79,35 @@ public class EmailControllerMockTest {
 		doReturn(message).when(controllerSpy).createMimeMessage();
 		doReturn(helper).when(controllerSpy).createMimeMessageHelper(message);
 		String html = "<p>Hello World!</p>";
-		doReturn(html).when(controllerSpy).processModelIntoHtml(model);
+		doReturn(html).when(controllerSpy).processModelIntoHtml(cartModel);
 		doNothing().when(controllerSpy).sendEmail(message);
-		controllerSpy.sendEmail("subject", model);
+		controllerSpy.sendEmail("subject", cartModel);
 		verify(controllerSpy).sendEmail(message);
 	}
 	
 	@Test
 	public void shouldReturnEmailFailureView() {
-		String actual = controller.emailFailure("error", model1);
-		assertThat(actual, Matchers.is("email-failure"));
+		String actual = controller.emailFailure("error", springModel);
+		assertThat(actual, is("email-failure"));
 	}
 	
 	@Test
 	public void shouldAttachTheErrorToTheModel() {
 		String error = "Off by 1";
-		controller.emailFailure(error, model1);
-		Mockito.verify(model1).addAttribute("error", error);
+		controller.emailFailure(error, springModel);
+		verify(springModel).addAttribute("error", error);
+	}
+	
+	@Test
+	public void shouldTellJavaMailSenderToSendAnEmail() {
+		controller.sendEmail(message);
+		verify(sender).send(message);
+	}
+	
+	@Test
+	public void shouldGetANewMimeMessageFromTheJavaMailSender() {
+		when(sender.createMimeMessage()).thenReturn(message);
+		MimeMessage actual = controller.createMimeMessage();
+		assertThat(actual, is(message));
 	}
 }
