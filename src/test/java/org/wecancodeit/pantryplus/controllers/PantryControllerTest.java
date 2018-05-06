57dc47c9215125ba80b3ed0c0a11b8bd77dca44d
alexjamesmalcolm <alexjamesmalcolm@gmail.com>
2018-05-06 16:50:03 -0400

    Added test for displaying the invalid zipcode view

--- src/test/java/org/wecancodeit/pantryplus/controllers/PantryControllerTest.java
+++ src/test/java/org/wecancodeit/pantryplus/controllers/PantryControllerTest.java
@@ -100,11 +100,18 @@ public class PantryControllerTest {
 		verify(model).addAttribute("categories", categories);
 		verify(model).addAttribute("cart", cart);
 	}
+	
 	@Test
 	public void shouldReturnInvalidZipCodeViewWhenGivenOtherAsZipCode() {
 		PantryController underTest = new PantryController();
 		String result = underTest.userFormProcessing(null, null, 0, 0, false, null, "Other", null, null);
 		assertThat(result, is("redirect:/invalid-zipcode"));
+	}
+	
+	@Test
+	public void shouldReturnInvalidZipCodeView() {
+		String templateName = underTest.displayIncorrectZipcode();
+		assertThat(templateName, is("invalid-zipcode"));
 	}
 	
 }
