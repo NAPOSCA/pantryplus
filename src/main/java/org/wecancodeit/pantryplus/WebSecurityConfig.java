3b33ae5fe50064df8c3fc71218a963869d32a3b3
theoccasionalist <mrmccall07@gmail.com>
2018-06-15 16:13:50 -0400

    added security dependency and added user/password

---
+++ src/main/java/org/wecancodeit/pantryplus/WebSecurityConfig.java
@@ -0,0 +1,24 @@
+package org.wecancodeit.pantryplus;
+
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    	auth.inMemoryAuthentication().withUser("user").password("password").roles("USER").and()
+    	.withUser("admin").password("password").roles("USER", "ADMIN");
+    }
+}
