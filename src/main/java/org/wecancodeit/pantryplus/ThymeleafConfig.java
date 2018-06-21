ee13b91d01add310c9d460a7d1cc38164e1144b4
alexjamesmalcolm <alexjamesmalcolm@gmail.com>
2018-06-21 02:20:55 -0400

    A hacky, probably broken fix for setting the templates of emails

--- src/main/java/org/wecancodeit/pantryplus/ThymeleafConfig.java
+++ src/main/java/org/wecancodeit/pantryplus/ThymeleafConfig.java
@@ -4,27 +4,27 @@ import java.nio.charset.StandardCharsets;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.thymeleaf.spring4.SpringTemplateEngine;
-import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
-import org.thymeleaf.templatemode.StandardTemplateModeHandlers;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 
 @Configuration
 public class ThymeleafConfig {
 
 	@Bean
-    public SpringTemplateEngine springTemplateEngine() {
-        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
-        templateEngine.addTemplateResolver(htmlTemplateResolver());
-        return templateEngine;
-    }
+	public SpringTemplateEngine springTemplateEngine() {
+		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+		templateEngine.addTemplateResolver(htmlTemplateResolver());
+		return templateEngine;
+	}
 
-    @Bean
-    public SpringResourceTemplateResolver htmlTemplateResolver(){
-        SpringResourceTemplateResolver emailTemplateResolver = new SpringResourceTemplateResolver();
-        emailTemplateResolver.setPrefix("classpath:/templates/");
-        emailTemplateResolver.setSuffix(".html");
-        emailTemplateResolver.setTemplateMode(StandardTemplateModeHandlers.HTML5.getTemplateModeName());
-        emailTemplateResolver.setCharacterEncoding(StandardCharsets.UTF_8.name());
-        return emailTemplateResolver;
-    }
+	@Bean
+	public SpringResourceTemplateResolver htmlTemplateResolver() {
+		SpringResourceTemplateResolver emailTemplateResolver = new SpringResourceTemplateResolver();
+		emailTemplateResolver.setPrefix("classpath:/templates/");
+		emailTemplateResolver.setSuffix(".html");
+		// emailTemplateResolver.setTemplateMode(StandardTemplateModeHandlers.HTML5.getTemplateModeName());
+		emailTemplateResolver.setTemplateMode("HTML");
+		emailTemplateResolver.setCharacterEncoding(StandardCharsets.UTF_8.name());
+		return emailTemplateResolver;
+	}
 }
