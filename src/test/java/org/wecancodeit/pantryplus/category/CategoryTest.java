fbc857352c6e9fd7567a98308ded85b78a2cdb5a
alexjamesmalcolm <alexjamesmalcolm@gmail.com>
2018-06-21 11:30:01 -0400

    Removed inappropriate test and reworked another

--- src/test/java/org/wecancodeit/pantryplus/category/CategoryTest.java
+++ src/test/java/org/wecancodeit/pantryplus/category/CategoryTest.java
@@ -1,28 +1,16 @@
 package org.wecancodeit.pantryplus.category;
 
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
-import java.util.Collection;
-
 import org.junit.Test;
-import org.wecancodeit.pantryplus.category.Category;
-import org.wecancodeit.pantryplus.product.Product;
 
 public class CategoryTest {
-	Product testProduct = new Product("testProduct", null);
-	Category testCategory = new Category("testName", testProduct);
 
 	@Test
 	public void shouldGetName() {
-		String checkName = testCategory.getName();
-		assertEquals(checkName, "testName");
-	}
-
-	@Test
-	public void shouldHaveProductInCategory() {
-		Collection<Product> checkProduct = testCategory.getProducts();
-		assertThat(testCategory.getProducts(), is(checkProduct));
+		Category underTest = new Category("testName");
+		String actualName = underTest.getName();
+		assertThat(actualName, is("testName"));
 	}
 }
