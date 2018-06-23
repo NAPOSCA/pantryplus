f7b31ded315839218271a54afcb2667f3c20c325
alexjamesmalcolm <alexjamesmalcolm@gmail.com>
2018-06-23 00:34:08 -0400

    Removed functionality from Category.java by removing the test that created it

--- src/test/java/org/wecancodeit/pantryplus/category/CategoryTest.java
+++ src/test/java/org/wecancodeit/pantryplus/category/CategoryTest.java
@@ -1,28 +1,24 @@
 package org.wecancodeit.pantryplus.category;
 
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
-import java.util.Collection;
-
 import org.junit.Test;
-import org.wecancodeit.pantryplus.category.Category;
-import org.wecancodeit.pantryplus.product.Product;
 
 public class CategoryTest {
-	Product testProduct = new Product("testProduct", null);
-	Category testCategory = new Category("testName", testProduct);
 
 	@Test
-	public void shouldGetName() {
-		String checkName = testCategory.getName();
-		assertEquals(checkName, "testName");
+	public void shouldGetNameFoo() {
+		Category underTest = new Category("foo");
+		String name = underTest.getName();
+		assertThat(name, is("foo"));
 	}
-
+	
 	@Test
-	public void shouldHaveProductInCategory() {
-		Collection<Product> checkProduct = testCategory.getProducts();
-		assertThat(testCategory.getProducts(), is(checkProduct));
+	public void shouldGetNameBar() {
+		Category underTest = new Category("bar");
+		String name = underTest.getName();
+		assertThat(name, is("bar"));
 	}
+	
 }
